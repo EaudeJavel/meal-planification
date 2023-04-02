d6ceca4e171a1eea17495d8f23fa6cafeadcad65
Paul Lamy <p.plamy6@gmail.com>
2023-04-02 21:34:46 +0200

    feat: day in backend + fix: strapi api keys

--- src/components/SelectMeal.jsx
+++ src/components/SelectMeal.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SelectMeal({ meals, onSelect, selectedDate }) {
   return (
     <div>
-      <h3>Choose your meal</h3>
+      <h3>Choose an existing meal</h3>
       {meals.map((meal) => (
         <button key={meal.id} onClick={() => onSelect(meal)}>
           {meal.attributes.name}
