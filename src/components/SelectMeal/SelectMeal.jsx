6167b4b89a9f493d75548ce56d0c25d8001dd180
Paul Lamy <p.plamy6@gmail.com>
2023-04-20 18:28:29 +0200

    chore: select existing meal fix

--- src/components/SelectMeal/SelectMeal.jsx
+++ src/components/SelectMeal/SelectMeal.jsx
@@ -1,15 +1,59 @@
-import React from 'react';
-import { SubHeading, Button, SelectMealContainer } from "./SelectMeal.styles";
+import React, { useState } from "react";
+import {
+  Input,
+  SelectMealContainer,
+  FilteredMealsContainer,
+  FilteredMeal,
+  SubmitButton,
+} from "./SelectMeal.styles";
 
 function SelectMeal({ meals, onSelect }) {
+  const [inputValue, setInputValue] = useState("");
+  const [isFilteredMealsVisible, setIsFilteredMealsVisible] = useState(false);
+
+  // Filter meals based on input value
+  const filteredMeals = meals.filter(
+    (meal) =>
+      meal.attributes.name.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
+  );
+
   return (
     <SelectMealContainer>
-      <SubHeading>Choisir une recette existante</SubHeading>
-      {meals.map((meal) => (
-        <Button key={meal.id} onClick={() => onSelect(meal)}>
-          {meal.attributes.name}
-        </Button>
-      ))}
+      <Input
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+          setIsFilteredMealsVisible(true);
+        }}
+        onFocus={() => setIsFilteredMealsVisible(true)}
+        onBlur={() => setIsFilteredMealsVisible(false)}
+      />
+      <FilteredMealsContainer isVisible={isFilteredMealsVisible}>
+        {filteredMeals.map((meal) => (
+          <FilteredMeal
+            key={meal.id}
+            onClick={() => {
+              setInputValue(meal.attributes.name);
+              setIsFilteredMealsVisible(false);
+            }}
+          >
+            {meal.attributes.name}
+          </FilteredMeal>
+        ))}
+      </FilteredMealsContainer>
+      <SubmitButton
+        onClick={() => {
+          const selectedMeal = meals.find(
+            (meal) => meal.attributes.name === inputValue
+          );
+          if (selectedMeal) {
+            onSelect(selectedMeal);
+          }
+        }}
+      >
+        Selectionner la recette
+      </SubmitButton>
     </SelectMealContainer>
   );
 }
--- src/components/SelectMeal/SelectMeal.styles.jsx
+++ src/components/SelectMeal/SelectMeal.styles.jsx
@@ -1,20 +1,14 @@
 import styled from 'styled-components';
 import { paleOrange, deepTaupe, milkChocolate, plumpPurple } from "../../styles";
 
-export const SubHeading = styled.h2`
-  font-size: 2rem;
-  font-weight: 700;
-  margin-bottom: 20px;
-  color: ${deepTaupe};
-`;
-
 export const Button = styled.button`
+  max-width: 250px;
+  min-height: 50px;
   font-size: 1rem;
   padding: 10px 20px;
   background-color: ${plumpPurple};
-  color: #fff;
+  color: ${paleOrange};
   border: none;
-  border-radius: 50px;
   cursor: pointer;
   transition: background-color 0.3s;
 
@@ -28,9 +22,67 @@ export const Button = styled.button`
   }
 `;
 
+
 export const SelectMealContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-bottom: 20px;
+  position: relative;
+`;
+
+export const Input = styled.input`
+  width: 200px;
+  height: 30px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+`;
+
+export const FilteredMealsContainer = styled.ul`
+  position: absolute;
+  top: 50px;
+  left: 0;
+  width: 100%;
+  margin: 0;
+  padding: 8px;
+  list-style: none;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  opacity: ${(props) => (props.isVisible ? 1 : 0)};
+  visibility: ${(props) => (props.isVisible ? "visible" : "hidden")};
+  transition: opacity 0.2s ease-out, visibility 0.2s ease-out;
+`;
+
+export const FilteredMeal = styled.li`
+  padding: 4px 0;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f6f6f6;
+  }
+`;
+
+export const SubmitButton = styled.button`
+  font-size: 1rem;
+  padding: 10px 20px;
+  margin-top: 10px;
+  background-color: ${plumpPurple};
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #ff6b5c;
+  }
+
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
 `;
