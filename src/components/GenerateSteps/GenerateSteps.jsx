a260f94d63a408b97a4330672f9b71d117af7516
Paul Lamy <p.plamy6@gmail.com>
2023-04-25 11:37:47 +0200

    fix: GenerateSteps api route

--- src/components/GenerateSteps/GenerateSteps.jsx
+++ src/components/GenerateSteps/GenerateSteps.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import * as api from "../components/Api";
+import * as api from "../Api";
 
 function GenerateSteps() {
   const [recipeName, setRecipeName] = useState("");
